123305a005563cbec2f6d1a93ee8fb2b33109b5a
Mettle X <56449790+mettlex@users.noreply.github.com>
2022-10-19 00:07:41 +0600

    wip: print encoded shares

--- cli/one_time_setup.ts
+++ cli/one_time_setup.ts
@@ -1,7 +1,8 @@
 import { combineShares, createShares } from "../sss-wasm/index.js";
-import { Number, Secret, Confirm } from "./prompt/mod.ts";
+import { Number, Secret, Confirm, Select } from "./prompt/mod.ts";
 import { encode, decode } from "./encoding/base64.ts";
 import { zeroPadBy64 } from "./_utils/zero-pad.ts";
+import { directoryExists } from "./_utils/fs.ts";
 
 const totalShareCount = await Number.prompt({
   message: "Total number of password shares",
@@ -124,8 +125,78 @@ if (secretFromInput) {
       Deno.exit(1);
     }
   }
+
+  console.log("\n\nThe shares have been generated successfully.\n\n");
+} else {
+  console.log(
+    "\n\nA random password and the shares have been generated successfully.\n\n",
+  );
 }
 
-console.log(
-  "\n\nA random password and the shares have been generated successfully.\n\n",
-);
+const dataDirectoryExists = await directoryExists("data");
+
+if (!dataDirectoryExists) {
+  Deno.mkdirSync("data");
+}
+
+const shouldEncryptSharesOneByOne = await Confirm.prompt({
+  message: "Do you want to encrypt the shares one by one?",
+  default: true,
+});
+
+if (!shouldEncryptSharesOneByOne) {
+  const shouldEncryptSharesWithSinglePassword = await Confirm.prompt({
+    message: "Do you want to encrypt the shares with a single password?",
+    default: false,
+  });
+
+  if (!shouldEncryptSharesWithSinglePassword) {
+    enum ExportTypes {
+      SAVE = "save",
+      PRINT = "print",
+    }
+
+    const exportType = (await Select.prompt({
+      message: "Select an option",
+      options: [
+        Select.separator("--------"),
+        { name: "Save the shares as files", value: ExportTypes.SAVE },
+        Select.separator("--------"),
+        { name: "Print the shares to the console", value: ExportTypes.PRINT },
+      ],
+    })) as ExportTypes;
+
+    if (exportType === ExportTypes.PRINT) {
+      const totalParts = secretsWithShares.length;
+      let secretCount = 1;
+
+      for (const { shares } of secretsWithShares) {
+        console.log(
+          `\n\n============== Secret (${secretCount}/${totalParts}) ==============\n`,
+        );
+
+        const totalShares = shares.length;
+
+        shares.forEach((share, i) => {
+          console.log(
+            `=== Share ${
+              i + 1
+            }/${totalShares} of Secret ${secretCount}/${totalParts} ===\n`,
+          );
+          console.log(encode(share));
+          console.log("\n\n");
+        });
+
+        console.log(
+          `\n========== End of Secret (${secretCount}/${totalParts}) ==========\n`,
+        );
+
+        secretCount++;
+      }
+    }
+
+    Deno.exit(0);
+  }
+
+  Deno.exit(0);
+}
